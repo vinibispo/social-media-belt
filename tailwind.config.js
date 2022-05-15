480032d4ca4a74a49c25773fa08f3c84ec5ab243
vinibispo <vini.bispo015@gmail.com>
2022-05-15 10:13:21 -0300

    feat(components): add components in content of tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,8 @@ module.exports = {
   content: [
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
+    './app/components/**/*.rb',
+    './app/components/**/*.html.erb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
   ]
