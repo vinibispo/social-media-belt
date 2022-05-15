b1d013c57422d6f72265da7d250f29535152eecd
vinibispo <vini.bispo015@gmail.com>
2022-05-15 11:00:27 -0300

    feat(routes): create app/links route

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,10 @@ Rails.application.routes.draw do
   get '/app', to: 'app/index#call', as: :app
   get '/:slug', to: 'tenant_home/index#call', as: :slug
   root 'home/index#call'
+
+  modular_routes do
+    namespace :app do
+      resources :links, only: %i[index]
+    end
+  end
 end
